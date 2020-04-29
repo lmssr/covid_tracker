9b38380f3bed03d531c0bf63032361f5adae3cd7
Luc Mosser <luc.mosser86@orange.fr>
2020-04-29 17:08:01 +0200

    adding some style

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css'
 
-const Chart = ({ data, country }) => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
   const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
@@ -13,9 +13,9 @@ const Chart = ({ data, country }) => {
     }
 
     fetchAPI();
-  });
+  }, []);
 
-  const lineCarth = (
+  const lineChart = (
     dailyData.length
     ? (<Line
       data={{
@@ -36,10 +36,33 @@ const Chart = ({ data, country }) => {
     />) : null
   );
 
+  const barChart = (
+    confirmed
+      ? (
+        <Bar
+          data={{
+            labels: ['Infected', 'Recovered', 'Deaths'],
+            datasets: [{
+              label: 'People',
+              backgroundColor: [
+                'rgba(0, 0, 255, 0.5)',
+                'rgba(0, 255, 0, 0.5)',
+                'rgba(255, 0, 0, 0.5)'
+                ],
+              data: [confirmed.value, recovered.value, deaths.value]
+            }]
+          }}
+          options={{
+            legend: { display:false },
+            title: { display: true, text:`Current state in ${country}`}
+          }}
+        />
+        ) : null
+    );
 
   return(
     <div className={styles.container}>
-      {lineCarth}
+      {country ? barChart : lineChart}
     </div>
     )
 }
